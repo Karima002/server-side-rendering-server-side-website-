f0b12dcdb6a2c4068671ef30ac5b6ed2d0546331
Karima002 <karima.mouadi@hva.nl>
2025-03-06 23:21:05 +0100

    #4 form, nav gestyled en gepostioneerd

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.get ('/object', async function (request, response) {
   
   response.render("objects.liquid", { api: apiResponseJSON.data });
 });
+
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
 app.post('/', async function (request, response) {
